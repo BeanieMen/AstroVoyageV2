ffd7c01af45c12745a203346dffe827cb95101ae
BeanieMen <jainarjav886@gmail.com>
2024-02-01 14:24:43 +0530

    feat: fix api req

--- app/api/apod/route.ts
+++ app/api/apod/route.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiResponse } from 'next'
+import { NextRequest } from 'next/server'
 export async function GET(
-    req: NextApiRequest,
+    req: NextRequest,
     res: NextApiResponse<object>
 ) {
     let response = await fetch('https://api.nasa.gov/planetary/apod?api_key=' + process.env.apod)
