319a526ac8307ff5a82bd2604fde16235eac79a6
Beanie <62057938+BeanieMen@users.noreply.github.com>
2024-02-04 08:48:35 +0000

    feat: add stars

--- app/solar-system/page.tsx
+++ app/solar-system/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React from 'react'
-import { Canvas, extend } from '@react-three/fiber'
+import React, { useEffect } from 'react'
+import { Canvas, extend, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Sun from './objects/sun'
 import Planet from './objects/planets'
@@ -9,16 +9,58 @@ import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import Navbar from '../components/NavBar'
 import { Leva, useControls } from 'leva'
 import PlaneteRing from './objects/planete-ring'
+import { CubeTextureLoader } from 'three'
 
 extend({ UnrealBloomPass })
 
-export default function Solar() {
+function Solar() {
 	const intensity = 0.1
-
+	const { scene } = useThree()
 	const { bloom } = useControls({
 		bloom: false,
 	})
+	useEffect(() => {
+		const loader = new CubeTextureLoader()
+		loader.load(
+			['/imgs/8k_stars.jpg', '/imgs/8k_stars.jpg', '/imgs/8k_stars.jpg', '/imgs/8k_stars.jpg', '/imgs/8k_stars.jpg', '/imgs/8k_stars.jpg'],
+			cubeTexture => {
+				scene.background = cubeTexture;
+			}
+		);
+	})
+	return (
 
+		<>
+			{bloom ? (<EffectComposer disableNormalPass>
+				<Bloom mipmapBlur={false} luminanceThreshold={0} levels={2} intensity={intensity * 4} />
+			</EffectComposer>) : (null)}
+
+
+			<OrbitControls />
+
+			<ambientLight intensity={1} />
+			<pointLight position={[0, 0, 0]} color={'white'} intensity={500} />
+
+			<Sun position={[0, 0, 0]} />
+			<Planet position={[0, 0, 0]} radius={5} image={'/imgs/mercury.jpg'} speed={1 / 5} size={0.3} />
+			<Planet position={[0, 0, 0]} radius={7} image={'/imgs/venus.jpg'} speed={0.6 / 5} size={0.35} />
+			<Planet position={[0, 0, 0]} radius={10} image={'/imgs/earth.jpg'} speed={0.4 / 5} size={0.5} />
+			<Planet position={[0, 0, 0]} radius={13} image={'/imgs/mars.jpg'} speed={0.3 / 5} size={0.4} />
+			<Planet position={[0, 0, 0]} radius={20} image={'/imgs/jupiter.jpg'} speed={0.2 / 5} size={0.7} />
+
+			<Planet position={[0, 0, 0]} radius={30} image={'/imgs/saturn.jpg'} speed={0.1 / 5} size={0.8} />
+			<PlaneteRing radius={30} inner={1} image={'/imgs/saturnRing.png'} outer={1.8} speed={0.1 / 5}></PlaneteRing>
+
+			<Planet position={[0, 0, 0]} radius={37} image={'/imgs/uranus.jpg'} speed={0.1 / 10} size={0.9} />
+			<Planet position={[0, 0, 0]} radius={45} image={'/imgs/neptune.jpg'} speed={0.1 / 15} size={0.9} />
+
+		</>
+
+
+	)
+}
+
+export default function System() {
 	return (
 		<div className='relative h-screen overflow-hidden select-none'>
 			<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 '>
@@ -26,35 +68,11 @@ export default function Solar() {
 			</div>
 			<div className='w-screen h-screen bg-black'>
 				<Navbar />
-				<Canvas >
-					{bloom ? (<EffectComposer disableNormalPass>
-						<Bloom mipmapBlur={false} luminanceThreshold={0} levels={2} intensity={intensity * 4} />
-					</EffectComposer>) : (null)}
-
-
-					<OrbitControls />
-
-					<ambientLight intensity={1} />
-					<pointLight position={[0, 0, 0]} color={'white'} intensity={500}  />
-
-					<Sun position={[0, 0, 0]} />
-					<Planet position={[0, 0, 0]} radius={5} image={'/imgs/mercury.jpg'} speed={1/5} size={0.3}/>
-					<Planet position={[0, 0, 0]} radius={7} image={'/imgs/venus.jpg'} speed={0.6/5} size={0.35}/>
-					<Planet position={[0, 0, 0]} radius={10} image={'/imgs/earth.jpg'} speed={0.4/5} size={0.5}/>
-					<Planet position={[0, 0, 0]} radius={13} image={'/imgs/mars.jpg'} speed={0.3/5} size={0.4}/>
-					<Planet position={[0, 0, 0]} radius={20} image={'/imgs/jupiter.jpg'} speed={0.2/5} size={0.7}/>
-
-					<Planet position={[0, 0, 0]} radius={30} image={'/imgs/saturn.jpg'} speed={0.1/5} size={0.8}/>
-					<PlaneteRing radius={30} inner={1} image={'/imgs/saturnRing.png'} outer={1.8} speed={0.1/5}></PlaneteRing>
-
-					<Planet position={[0, 0, 0]} radius={37} image={'/imgs/uranus.jpg'} speed={0.1/10} size={0.9}/>
-					<Planet position={[0, 0, 0]} radius={45} image={'/imgs/neptune.jpg'} speed={0.1/15} size={0.9}/>
-
+				<Canvas>
+					<Solar />
 				</Canvas>
+
 			</div>
-
-
 		</div>
-
 	)
 }
