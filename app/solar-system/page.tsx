58fdea4bacd4aa53764a8c277b8eebe98014aeca
BeanieMen <jainarjav886@gmail.com>
2024-02-04 12:14:56 +0530

    feat: enable or disable bloom

--- app/solar-system/page.tsx
+++ app/solar-system/page.tsx
@@ -6,27 +6,44 @@ import Sun from './objects/sun'
 import Planet from './objects/planets'
 import Ring from './objects/ring'
 import { UnrealBloomPass } from 'three-stdlib'
-import { ToneMappingMode } from 'postprocessing'
-import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import Navbar from '../components/NavBar'
+import { Leva, useControls } from 'leva'
 
-extend({UnrealBloomPass})
+extend({ UnrealBloomPass })
 
 export default function Solar() {
 	const intensity = 0.1
 
+	const { bloom } = useControls({
+		bloom: false,
+	})
 	return (
-		<div className='w-screen h-screen bg-black'>
-			<Canvas >
-			<EffectComposer disableNormalPass>
-          <Bloom mipmapBlur luminanceThreshold={0.35} levels={3} intensity={intensity * 4} />
-        </EffectComposer>
-				<OrbitControls />
-				<ambientLight intensity={0.8} />
-				<pointLight position={[0, 0, 0]} color={'white'} intensity={350} distance={300} />
-				<Sun position={[0, 0, 0]} />
-				<Ring/>
-				<Planet position={[0,0,0]} radius={5} image={'/imgs/earth.jpg'}/>
-			</Canvas>
+		<div className='relative h-screen'>
+
+			<div className='w-screen h-screen bg-black'>
+
+				<Navbar />
+				<Canvas >
+					{bloom ? (<EffectComposer disableNormalPass>
+						<Bloom mipmapBlur={false} luminanceThreshold={0.35} levels={3} intensity={intensity * 4} />
+					</EffectComposer>) : (null)}
+
+
+					<OrbitControls />
+
+					<ambientLight intensity={0.8} />
+					<pointLight position={[0, 0, 0]} color={'white'} intensity={350} distance={300} />
+
+					<Sun position={[0, 0, 0]} />
+					<Ring />
+					<Planet position={[0, 0, 0]} radius={5} image={'/imgs/earth.jpg'} />
+				</Canvas>
+			</div>
+			<div className='absolute top-[92.5%] left-1/2 transform -translate-x-[92.5%] -translate-y-1/2 z-10'>
+				<Leva fill />
+			</div>
 		</div>
+
 	)
 }
