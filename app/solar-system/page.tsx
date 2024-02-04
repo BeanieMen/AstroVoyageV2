f125d399e4af4ebbf5ff17e7aaab5aad758a4127
Beanie <62057938+BeanieMen@users.noreply.github.com>
2024-02-04 07:34:11 +0000

    ix: workflow

--- app/solar-system/page.tsx
+++ app/solar-system/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Canvas, extend } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Sun from './objects/sun'
